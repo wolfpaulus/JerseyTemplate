08298c94a0d120975ae8a3b5a594c06078b4d6d1
Wolf Paulus <wolf@wolfpaulus.com>
2015-05-11 11:52:54 -0700

    app declaration and example response (echo)

---
+++ src/main/java/com/techcasita/jersey/Echo.java
@@ -0,0 +1,20 @@
+package com.techcasita.jersey;
+
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("echo")
+public class Echo {
+
+    @GET
+    @Produces("text/plain")
+    public String echo(@QueryParam("m") final String message) {
+        Logger.getLogger(getClass()).info("Request received with m=" + message);
+        return "echo: " + message;
+    }
+
+}
